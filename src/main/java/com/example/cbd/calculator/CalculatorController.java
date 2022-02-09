17a5600445c233f0d42b5657472e3069b9a65382
Hoang <bud.h@gmx.de>
2022-02-09 17:31:58 +0100

    second

--- src/main/java/com/example/cbd/calculator/CalculatorController.java
+++ src/main/java/com/example/cbd/calculator/CalculatorController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "api/v1/product")
+@RequestMapping(path = "api/v1/calculate")
 public class CalculatorController {
 
     private final CalculatorService calculatorService;
